7671c78d2031d24f1423e53ae7b72929ba92e860
Surafel Workayehu <surafelwork32@gmail.com>
2023-03-01 16:38:24 +0300

    updated the front-end and updated index.js back

--- client/client.js
+++ client/client.js
@@ -1,8 +1,10 @@
 const form=document.querySelector('form');
-
+const displayall=document.querySelector('.display-all');
+const getAll=document.getElementById('getall');
 const loading=document.querySelector('.loading');
 
-const api_url='http://localhost:5000/tot'
+const api_url='http://localhost:5000/tot';
+const url='http://localhost:5000/';
 loading.style.display='none'
 form.addEventListener('submit',(event)=>{
     event.preventDefault();
@@ -15,8 +17,8 @@ form.addEventListener('submit',(event)=>{
         thought
     }
 
-    form.style.display='none'
-    loading.style.display=''
+    // form.style.display='none'
+    // loading.style.display=''
     fetch(api_url,{
         method:'POST',
         body:JSON.stringify(pos),
@@ -25,9 +27,32 @@ form.addEventListener('submit',(event)=>{
         }
     }).then(response =>response.json()).
     then(createdTot=>{
+        form.reset();
         console.log(createdTot)
-        form.reset()
-        form.style.display='';
-        loading.style.display='none'
+        
+        
+        // loading.style.display='none'
     })
+    
 }) 
+
+async function getapi() {
+    
+    // Storing response
+    const response = await fetch('http://localhost:5000/');
+    displayall.innerHTML=""
+    // Storing data in form of JSON
+    var data = await response.json();
+    data.forEach(element => {
+        console.log(element.name,element.thought)
+        html=`<div class="inner-display">
+                <h2>${element.name}</h2>
+                <p>${element.thought}</p>
+            </div>`
+            displayall.innerHTML+=html;
+    });
+    
+    // to do for tommorow including the css 
+}
+
+getAll.addEventListener('click',getapi)
--- server/index.js
+++ server/index.js
@@ -1,47 +1,63 @@
-const express=require('express');
-const cors=require('cors');
-const mongoose=require('mongoose')
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2/promise');
 
-const app=express()
+const app = express();
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'thought',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
-
-
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.get('/',(req,res)=>{
-    res.json({
-        message:'My thought'
-    })
-})
 
-function isValid(tot){
-    return tot.name && tot.name.toString().trim()!=='' &&
-    tot.thought && tot.thought.toString().trim()!=='';
+app.get('/', async (req, res) => {
+  try {
+    const connection = await pool.getConnection();
+    const [rows, fields] = await connection.execute('SELECT name, thought FROM thoughts');
+    connection.release();
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+});
+
+function isValid(tot) {
+  return tot.name && tot.name.toString().trim() !== '' &&
+    tot.thought && tot.thought.toString().trim() !== '';
 }
 
-app.post('/tot',(req,res)=>{
-    console.log(req.body)
-    if (isValid(req.body)){
-        
-        const tot={
-            name:req.body.name.toString(),
-            thought:req.body.thought.toString()
-        }
-        // tots.insert(tot).then(createdTot=>{
-        //     res.json(createdTot)
-        // })
-        // console.log(tot)
-        
+app.post('/tot', async (req, res) => {
+  console.log(req.body);
+  if (isValid(req.body)) {
+    const tot = {
+      name: req.body.name.toString(),
+      thought: req.body.thought.toString()
+    };
+    try {
+      const connection = await pool.getConnection();
+      const [result, fields] = await connection.execute('INSERT INTO thoughts (name, thought) VALUES (?, ?)', [tot.name, tot.thought]);
+      connection.release();
+      console.log('Number of records inserted:', result.affectedRows);
+      res.sendStatus(200);
+    } catch (err) {
+      console.error(err);
+      res.sendStatus(500);
     }
-    else{
-        res.status(422)
-        return res.json({
-            message:'Both name and thought are required!!'
-        })
-    }
-    
-})
-app.listen(5000,()=>{
-    // connect()
-    console.log("listening on http://localhost:5000");
-})
+  } else {
+    res.status(422);
+    return res.json({
+      message: 'Both name and thought are required!!'
+    });
+  }
+});
+
+app.listen(5000, () => {
+  console.log('listening on http://localhost:5000');
+});
